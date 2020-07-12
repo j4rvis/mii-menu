aa83e1bb310b283c0036fe3fc26fab9f9b02ec30
Michael Schwarz <michael.schwarz@tui.com>
2020-07-12 18:33:08 +0200

    First data

--- src/_data/data.js
+++ src/_data/data.js
@@ -1,7 +1,190 @@
+
 module.exports = {
     "categories": [
         {
-            "name": "Hauptspeise",
+            "name": "Suppen & Eintöpfe",
+            "description": "",
+            "dishes": [
+                {
+                    "name": "Chilli con Carne",
+                    "description": ""
+                },
+                {
+                    "name": "Kürbiscremesuppe",
+                    "description": ""
+                },
+                {
+                    "name": "Erbsensuppe",
+                    "description": ""
+                },
+                {
+                    "name": "Kartoffelsuppe",
+                    "description": ""
+                },
+                {
+                    "name": "Bauerneintopf",
+                    "description": ""
+                },
+                {
+                    "name": "Linsensuppe",
+                    "description": ""
+                },
+                {
+                    "name": "Indische Linsensuppe",
+                    "description": ""
+                },
+                {
+                    "name": "Maultaschensuppe",
+                    "description": ""
+                },
+            ]
+        },
+        {
+            "name": "Salate",
+            "description": "",
+            "dishes": [
+                {
+                    "name": "Tomate & Mozzarella Salate",
+                    "description": ""
+                },
+                {
+                    "name": "Taco Salat",
+                    "description": ""
+                },
+                {
+                    "name": "Cous Cous Salat",
+                    "description": ""
+                },
+                {
+                    "name": "Schwäbischer Kartoffelsalat",
+                    "description": ""
+                },
+                {
+                    "name": "Italienischer Nudelsalat",
+                    "description": "mit Basilikumpesto, Pinienkernen und Parmesan"
+                },
+                {
+                    "name": "Wurstsalat",
+                    "description": ""
+                },
+                {
+                    "name": "Glasnudelsalat",
+                    "description": ""
+                },
+            ]
+        },
+        {
+            "name": "Snacks & Dips",
+            "description": "",
+            "dishes": [
+                {
+                    "name": "Hummus",
+                    "description": ""
+                },
+                {
+                    "name": "Guacamole",
+                    "description": ""
+                },
+                {
+                    "name": "Gemüse Sticks",
+                    "description": ""
+                },
+            ]
+        },
+        {
+            "name": "Schnelle Küche",
+            "description": "",
+            "dishes": [
+                {
+                    "name": "Gebratene Maultaschen",
+                    "description": ""
+                },
+                {
+                    "name": "Gnocchi mit Pesto",
+                    "description": ""
+                },
+                {
+                    "name": "Nudeln mit Spinat",
+                    "description": "kann auch mit Spiegelei serviert werden"
+                },
+                {
+                    "name": "Sandwichtoast",
+                    "description": ""
+                },
+                {
+                    "name": "Rührei mit Brot",
+                    "description": ""
+                },
+                {
+                    "name": "Omelette",
+                    "description": ""
+                },
+                {
+                    "name": "Flammkuchen",
+                    "description": ""
+                },
+                {
+                    "name": "Selbstgemachte Pizza",
+                    "description": "mit dem Pizza Baukasten"
+                },
+            ]
+        },
+        {
+            "name": "Gesunde Küche",
+            "description": "",
+            "dishes": [
+                {
+                    "name": "Entrecote mit Rosmarinkartoffeln und Brokolli mit Sauce Hollondaise",
+                    "description": ""
+                },
+            ]
+        },
+        {
+            "name": "Wohlfühl Essen",
+            "description": "",
+            "dishes": [
+                {
+                    "name": "Eierkuchen mit Champignon-Sahne-Sauce und Blaubeeren",
+                    "description": ""
+                },
+                {
+                    "name": "Lasagne",
+                    "description": ""
+                },
+                {
+                    "name": "Spaghetti Bologneseragout",
+                    "description": ""
+                },
+                {
+                    "name": "Steak and Ale Pie",
+                    "description": ""
+                },
+                {
+                    "name": "Rinderrouladen mit Glühweinsauce",
+                    "description": ""
+                },
+                {
+                    "name": "Kohlrouladen",
+                    "description": ""
+                },
+                {
+                    "name": "Entrecote mit Rosmarinkartoffeln und Brokolli mit Sauce Hollondaise",
+                    "description": ""
+                },
+            ]
+        },
+        {
+            "name": "Desserts",
+            "description": "",
+            "dishes": [
+                {
+                    "name": "Lavecake",
+                    "description": ""
+                }
+            ]
+        },
+        {
+            "name": "Getränke",
             "description": "",
             "dishes": [
                 {
